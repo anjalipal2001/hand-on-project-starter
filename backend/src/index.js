9894c1e3fe2e85800bc30de68d4368706da07bc6
anjalipal2001 <ap7197339@gmail.com>
2022-01-18 21:54:03 +0530

    server and database intialize

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+
 const app = express();
-mongoose 
-    .connect(proces.env.MONGODB_URL)
-    .then(()=>{
-        console.log("Connected to mongo db database");
-    })
-    .catch((err)=>{
-        console.log("Error connecting to database "+err);
-    });
-app.get("/hello-world",(req , res) => {
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("Connected to mongo db database");
+})
+.catch((err)=>{
+        console.log("Error connecting to database "+ err);
+ });
+app.get("/hello-world", (req, res) => {
     res.send("Hello world");
 });
-app.listen(process.env.PORT,()=>{
-    console.log("Backend server has started at"+ process.env.PORT)
+app.listen(process.env.PORT, () => { console.log("Backend server has started at" + process.env.PORT)
 });
